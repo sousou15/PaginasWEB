f85a0ba1166ecc918aa33b0225d545bc444b110f
sousou15 <43934641+sousou15@users.noreply.github.com>
2023-10-05 20:28:14 +0200

    Add files via upload
    ej 2 empezado

--- tareaDOM/scripts.js
+++ tareaDOM/scripts.js
@@ -1,19 +1,69 @@
 window.onload = function() { 
      // Obtener referencias a elementos del DOM
-     const enlace = document.getElementById('miEnlace');
-     const mostrarInfoBtn = document.getElementById('mostrarInfo');
-     const cambiarEnlaceBtn = document.getElementById('cambiarEnlace');
-     const  Mostrado = document.getElementById("Mostrado");
-     const Cambiado = document.getElementById('Cambiado');
+    const enlace = document.getElementById('miEnlace');
+    const mostrarInfoBtn = document.getElementById('mostrarInfo');
+    const cambiarEnlaceBtn = document.getElementById('cambiarEnlace');
+    const  Mostrado = document.getElementById("Mostrado");
+    const Cambiado = document.getElementById('Cambiado');
+     
+
+    const Incrementar = document.getElementById('Sumar');
+    const Decrementar = document.getElementById('Restar');
+    const Contador = document.getElementById('Contador');
 
 
-     const Incrementar = document.getElementById('Sumar');
-     const Decrementar = document.getElementById('Restar');
-     const Contador = document.getElementById('Contador');
+    const tabla = document.querySelector('table');
+    const dimensionBtn = document.getElementById('Dimensiones');
+    const BordeBtn = document.getElementById('Borde'); 
+    const alineacionBtn = document.getElementById('Alineacion');
+
+    if(dimensionBtn){ 
+        dimensionBtn.addEventListener('click', function() {
+            const anchoTabla = tabla.offsetWidth;
+            const altoTabla = tabla.offsetHeight;
+            alert(`Ancho de la tabla: ${anchoTabla}px\nAlto de la tabla: ${altoTabla}px`);
+            let nuevoancho, nuevoalto;
+            nuevoancho = parseFloat(prompt("Ingrese el primer valor:"));
+            nuevoalto = parseFloat(prompt("Ingrese el segundo valor:"));
+            if (!isNaN(nuevoancho) && !isNaN(nuevoalto)) {
+                // Aplicar las nuevas medidas a la tabla
+                tabla.style.width = `${nuevoancho}px`;
+                tabla.style.height = `${nuevoalto}px`;
+                
+                alert('Medidas actualizadas con éxito.');
+            } else {
+                alert('Por favor, ingrese medidas válidas (números enteros).');
+            }
+        });
+    }
+
+    if(BordeBtn){ 
+        BordeBtn.addEventListener('click', function() {
+            const estiloTabla = window.getComputedStyle(tabla);
+            const anchoBorde = estiloTabla.getPropertyValue('border-width');
+            alert(`Borde de la tabla: ${anchoBorde}`);
+            const nuevoBorde = parseInt(prompt("Ingrese nuevo valor:"));
+            if (!isNaN(nuevoBorde)) {
+                // Aplicar las nuevas medidas a la tabla
+                tabla.style.borderWidth = `${nuevoBorde}px`;
+                alert('Borde actualizado con éxito.');
+            } else {
+                alert('Por favor, ingrese una medida válidas (números enteros).');
+            }
+        });
+    }
+
+    if(alineacionBtn){
+        alineacionBtn.addEventListener('click', function() {
+            tabla.style.textAlign = 'right';
+        });
+    }
+
+
 
     /*Compruebo que el elemento existe y no es nulo, sino me da error al usar el mismo script en otro html no correspondiente : "Cannot read property 'addEventListener' of null".
     Este error  se produce cuando intentas agregar un evento a un elemento que no existe en la página actual y aparece en la consola del navegador. CTRL +SHIFT+J*/
-    
+
     if(Incrementar){ 
         Incrementar.addEventListener('click', function() {
             let numero = parseInt(Contador.textContent);
